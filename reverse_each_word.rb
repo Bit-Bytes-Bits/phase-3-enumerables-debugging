466b5c9e5d7ca9a1fa8e589a67de86debf876629
Ian Mike <ian.kaibi@student.moringaschool.com>
2022-10-20 09:43:55 +0300

    code along

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,24 @@
 # Write your code here
+require "pry"
+
+def reverse_each_word(sentence)
+  words = sentence.split(" ")
+  reversed_words = []
+  words.each { |word|
+    reversed_words << word.reverse
+  }
+#   binding.pry
+  reversed_words.join(" ")
+end
+
+puts reverse_each_word("Hello there, and how are you?")
+
+
+def reverse_each_word2(sentence)
+  words = sentence.split(" ")
+  # look into : Variations on Enumerable Syntax
+  reversed_words = words.map(&:reverse)
+  reversed_words.join(" ")
+end
+
+puts reverse_each_word2("Hello world, I am awesome")
